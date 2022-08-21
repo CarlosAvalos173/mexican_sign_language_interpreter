d436ec93acb9e4fea978c60668d52d83e02d0a21
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-21 11:29:28 -0500

    todo el proyecto equisde
    se sube todo el proyecto en este commit equisde
    
    Co-Authored-By: CarlosAvalos173 <56316980+CarlosAvalos173@users.noreply.github.com>
    Co-Authored-By: 1930346_Rob_FortnitePlayer <56317498+1930346@users.noreply.github.com>
    Co-Authored-By: Jesús Quiñones <56327734+yisuslalala@users.noreply.github.com>

--- dataset_generator/code/main.py
+++ dataset_generator/code/main.py
@@ -1,25 +1,65 @@
 # base app for generating the dataset
 # Authors: Agustin Zavala, Jose Avalos, Roberto Higuera, Jesus Quiñones
 
+import os
+import shutil
+from modules.data_loader import load_data_numpy
+from modules.neural_network_training import train_nn
 
-def generate_dataset():
+
+def copy_original_dataset() -> None:
+    for folder in os.listdir("dataset_generator/original_dataset/data"):
+        # create folder if it doesn't exist
+        if not os.path.exists("dataset_generator/dataset/" + folder):
+            os.mkdir("dataset_generator/dataset/" + folder)
+        number_of_existing_files = len(
+            os.listdir(f"dataset_generator/dataset/{folder}")
+        )
+        for file in os.listdir(f"dataset_generator/original_dataset/data/{folder}"):
+            number_of_existing_files += 1
+            shutil.copyfile(
+                src=f"dataset_generator/original_dataset/data/{folder}/{file}",
+                dst=f"dataset_generator/dataset/{folder}/{number_of_existing_files}.csv",
+            )
+
+
+def generate_dataset() -> None:
     print("generating the dataset")
-    pass
+    shutil.rmtree("dataset_generator/dataset")
+    os.mkdir("dataset_generator\dataset")
+
+    # check if videos folder exists
+    if os.path.exists("dataset_generator\\videos"):
+        print("Generating txt from videos")
+
+    if os.path.exists("dataset_generator\original_dataset\data"):
+        print("Copying original dataset")
+        copy_original_dataset()
+
+    x_train, y_train = load_data_numpy("dataset_generator/dataset")
 
 
-def train_model():
-    print("training the model")
-    pass
+def save_tflite_model() -> None:
+    import tensorflow as tf
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(
+        tf.keras.models.load_model("dataset_generator\\best_model.h5")
+    )
+    tflite_model = converter.convert()
+
+    with open("dataset_generator\\model.tflite", "wb") as f:
+        f.write(tflite_model)
 
 
-def main():
+def main() -> None:
     option = input("1.- generate the dataset\n2.- train the model\n3.- exit\n>> ")
 
     while option != "3":
         if option == "1":
             generate_dataset()
         elif option == "2":
-            train_model()
+            train_nn()
+            save_tflite_model()
         else:
             print("invalid option")
         option = input("1.- generate the dataset\n2.- train the model\n3.- exit\n>> ")
---
+++ dataset_generator/code/modules/txtextracter.py
@@ -0,0 +1,370 @@
+import time
+import cv2
+import mediapipe as mp
+import os
+import csv
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+final_face_data = []
+final_pose_data = []
+final_hand_data = []
+word_bank = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "ñ",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "w",
+    "y",
+    "z",
+    "none",
+    "gracias",
+    "de_nada",
+    "por_favor",
+    "hola",
+]
+data = []
+word_list = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "ñ",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "w",
+    "y",
+    "z",
+    "none",
+    "gracias",
+    "de_nada",
+    "por_favor",
+    "hola",
+]
+currentPosition = 0
+word = word_list[currentPosition]
+iteration = 0
+
+
+# https://google.github.io/mediapipe/solutions/holistic
+
+
+def record_data() -> None:
+    """Saves raw data to the appropiate location"""
+    global iteration, final_hand_data, final_face_data, final_pose_data, currentPosition, word
+    print("trying to print " + word + str(iteration))
+    with open("Hands/" + word + "/" + word + str(iteration) + ".txt", "w+") as f:
+        write = csv.writer(f)
+        write.writerows(final_hand_data)
+    with open(
+        "DataOriginal/Face/" + word + "/" + word + str(iteration) + ".txt", "w+"
+    ) as f:
+        write = csv.writer(f)
+        write.writerows(final_face_data)
+    with open("Torso/" + word + "/" + word + str(iteration) + ".txt", "w+") as f:
+        write = csv.writer(f)
+        write.writerows(final_pose_data)
+    final_hand_data = []
+    iteration += 1
+    word = word_list[currentPosition]
+    if iteration == len(get_ready_words("/videos/{word}/")) + 11:
+        iteration = len(get_ready_words("/videos/{word}/"))
+        currentPosition += 1
+
+    final_face_data = []
+    final_pose_data = []
+
+
+def get_ready_words(directory: str) -> None:
+    """Get file names from the current directory"""
+    existing_words = os.listdir(directory)
+    return existing_words
+
+
+def live_data_extracter() -> None:
+    """Uses webcam feed to extract both hands coordinates as described in this image: https://google.github.io/mediapipe/images/mobile/hand_landmarks.png"""
+    global final_hand_data, final_face_data, final_pose_data, currentPosition, word, iteration
+    hand_data_frameR = []
+    face_data = []
+    hand_data_frameL = []
+    pose_data = []
+    frame = 0
+    frame_rate = 12
+    prev = 0
+    begin = False
+    end = False
+    iteration = len(get_ready_words("/videos/{word}/"))
+    cap = cv2.VideoCapture(0)
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+        while cap.isOpened():
+            time_elapsed = time.time() - prev
+            success, image = cap.read()
+
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            if time_elapsed > 1.0 / frame_rate:
+                prev = time.time()
+
+                # To improve performance, optionally mark the image as not writeable to
+                # pass by reference.
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = holistic.process(image)
+
+                # Draw landmark annotation on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_holistic.POSE_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+                )
+                # for hand_no,hand_landmarks in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.left_hand_landmarks,
+                    mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style(),
+                )
+
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.right_hand_landmarks,
+                    mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style(),
+                )
+
+                if begin == True:
+                    frame += 1
+
+                    hand_data_frameR.append(str(frame))
+                    hand_data_frameR.append("right")
+                    if results.right_hand_landmarks is not None:
+                        for hand_landmarks in results.right_hand_landmarks.landmark:
+                            hand_data_frameR.append(str(hand_landmarks.x))
+                            hand_data_frameR.append(str(hand_landmarks.y))
+                            hand_data_frameR.append(str(hand_landmarks.z))
+                        # final_hand_data.append(hand_data_frameR)
+                        # hand_data_frameR = []
+                    else:
+                        for i in range(63):
+                            hand_data_frameR.append(0)
+                    final_hand_data.append(hand_data_frameR)
+                    hand_data_frameR = []
+
+                    hand_data_frameL.append(str(frame))
+                    hand_data_frameL.append("left")
+                    if results.left_hand_landmarks is not None:
+                        for hand_landmarks in results.left_hand_landmarks.landmark:
+                            hand_data_frameL.append(str(hand_landmarks.x))
+                            hand_data_frameL.append(str(hand_landmarks.y))
+                            hand_data_frameL.append(str(hand_landmarks.z))
+
+                        # final_hand_data.append(hand_data_frameL)
+                        # hand_data_frameL = []
+                    else:
+                        for i in range(63):
+                            hand_data_frameL.append(0)
+                    final_hand_data.append(hand_data_frameL)
+                    hand_data_frameL = []
+
+                    for i in range(0, 11):
+                        face_data.append(str(results.pose_landmarks.landmark[i].x))
+                        face_data.append(str(results.pose_landmarks.landmark[i].y))
+                        face_data.append(str(results.pose_landmarks.landmark[i].z))
+                    final_face_data.append(face_data)
+                    face_data = []
+                    for i in range(11, 13):
+                        pose_data.append(str(results.pose_landmarks.landmark[i].x))
+                        pose_data.append(str(results.pose_landmarks.landmark[i].y))
+                        pose_data.append(str(results.pose_landmarks.landmark[i].z))
+                    final_pose_data.append(pose_data)
+                    pose_data = []
+                    if frame == 36:
+                        # if end == True:
+                        frame = 0
+                        begin = False
+                        # end = False
+                        record_data()
+                # Flip the image horizontally for a selfie-view display.
+                cv2.imshow("MediaPipe Holistic", cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == ord("a"):
+                    if begin == False:
+                        print(word)
+                        begin = True
+                    # else:
+                    #   end = True
+
+    cap.release()
+
+
+def unique_values(list_a: list, list_b: list) -> set:
+    """
+    Compares to lists and gets the values in one but not in the other.
+
+    Args:
+
+        list_a (list): Base list to compare.
+        list_b (list): Elements in second list to substract.
+
+
+    Returns:
+      set: Set containing values that exist in the first list but not the second.
+    """
+    a = set(list_a)
+    b = set(list_b)
+
+    return a - b
+
+
+def record_video() -> None:
+    """Record a video and show the wireframe overlay"""
+    global final_hand_data, final_face_data, final_pose_data, currentPosition, word
+
+    frame = 0
+    frame_rate = 12
+    prev = 0
+    index = 0
+    begin = False
+    cap = cv2.VideoCapture(0)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    available_directories = get_ready_words(f"dataset_generator/videos/")
+
+    for i in unique_values(word_bank, get_ready_words("dataset_generator/videos/")):
+        os.mkdir(f"dataset_generator/videos/{i}")
+    available_directories = get_ready_words("dataset_generator/videos/")
+    folder = available_directories[index]
+
+    available_words = get_ready_words(f"dataset_generator/videos/{folder}/")
+
+    iteration = len(available_words) + 1
+
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+        while cap.isOpened():
+            time_elapsed = time.time() - prev
+            success, image = cap.read()
+            image_to_add = image
+
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            if time_elapsed > 1.0 / frame_rate:
+                prev = time.time()
+
+                # To improve performance, optionally mark the image as not writeable to
+                # pass by reference.
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = holistic.process(image)
+
+                # Draw landmark annotation on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_holistic.POSE_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+                )
+                # for hand_no,hand_landmarks in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.left_hand_landmarks,
+                    mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style(),
+                )
+
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.right_hand_landmarks,
+                    mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style(),
+                )
+
+                if begin == True:
+                    frame += 1
+                    out.write(image_to_add)
+                    if frame == 36:
+                        frame = 0
+                        begin = False
+                        out.release()
+                        if iteration == len(available_words) + 11:
+                            folder = available_directories[index]
+                            available_words = get_ready_words(
+                                f"dataset_generator/videos/{folder}/"
+                            )
+                            iteration = len(available_words)
+                            index += 1
+                        iteration += 1
+                        print("Finished Recording")
+
+                cv2.imshow("MediaPipe Holistic", cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == ord("a"):
+                    if begin == False:
+                        begin = True
+                        out = cv2.VideoWriter(
+                            f"dataset_generator/videos/{folder}/{word}{iteration}.avi",
+                            cv2.VideoWriter_fourcc(*"DIVX"),
+                            12,
+                            (width, height),
+                        )
+
+
+if __name__ == "__main__":
+    record_video()
+    pass
